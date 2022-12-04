50ef306f8d46e50a8ed72d12b010faec4f76725b
Weronika Dziua <wer.dziura@gmail.com>
2022-12-04 15:45:24 +0100

    feat: work on testimonials popup for mobile and tablet width

--- pages/main/js/main.js
+++ pages/main/js/main.js
@@ -21,7 +21,6 @@ function hideMenu() {
 burgerOpacity.addEventListener('click', hideMenu)
 closeBurger.addEventListener('click', hideMenu)
 
-
 //slider pets
 
 const arrowOne = document.querySelector('.arrow1')
@@ -30,10 +29,50 @@ const mainSlider = document.getElementById('slider-main')
 const cards = mainSlider.getElementsByClassName('card')
 
 function slider() {
-    mainSlider.append(cards[0], cards[1], cards[2], cards[3], cards[4])
+	mainSlider.append(cards[0], cards[1], cards[2], cards[3], cards[4])
 }
-
-
 
 arrowOne.addEventListener('click', slider)
 arrowTwo.addEventListener('click', slider)
+
+// testimonials mobile
+let width = Math.max(window.screen.width, window.innerWidth)
+const blocks = document.querySelectorAll('.mobile-opinion')
+const testimonialOne = document.querySelector('#testimonial1')
+const testimonialTwo = document.querySelector('#testimonial2')
+const testimonialThree = document.querySelector('#testimonial3')
+const shadedBody = document.querySelector('.shade')
+
+let unshrinked
+
+function unshrinkTestimonial() {
+	unshrinked = document.querySelector('.unshrinked')
+	if (unshrinked) {
+		unshrinked.classList.remove('unshrinked')
+        unshrinked.style.height = '109px'
+        shadedBody.classList.remove('shaded-body')
+	}
+	this.classList.add('unshrinked')
+	this.style.display = 'flex'
+    this.style.height = '391px'
+    this.style.width = '294px'
+    shadedBody.classList.add('shaded-body')
+}
+
+function unshadeBody() {
+    unshrinked = document.querySelector('.unshrinked')
+    if(unshrinked) {
+        unshrinked.classList.remove('unshrinked')
+        this.classList.remove('shaded-body')
+        unshrinked.style.height = '109px'
+    }
+		
+        if (width >= 640 || width < 1000) {
+            unshrinked.style.width = '600px'
+        } if (width < 640) {
+            unshrinked.style.width = '299px'
+        }
+}
+
+blocks.forEach(block => block.addEventListener('click', unshrinkTestimonial))
+shadedBody.addEventListener('click', unshadeBody)
