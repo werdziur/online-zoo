7f91f10721615af5950a0d74d0a28aeaa52bb72d
Weronika Dziua <wer.dziura@gmail.com>
2022-12-09 01:11:31 +0100

    feat: start working on checking/unchecking input on the scale

--- pages/donate/donate.js
+++ pages/donate/donate.js
@@ -40,6 +40,7 @@ closeBurger.addEventListener('click', hideMenu)
 
 //input amount
 const inputScale = document.querySelector('.scale-input')
+let inputChecked =  document.querySelectorAll('.scale-inputs input[name="amount-of-money"]')
 const amount1 = document.querySelector('#amount-5000')
 const amount2 = document.querySelector('#amount-2000')
 const amount3 = document.querySelector('#amount-1000')
@@ -52,45 +53,56 @@ const amount8 = document.querySelector('#amount-25')
 function changeInputAmount(e) {
 	const previous = document.querySelector('.round-active')
 	const previous2 = document.querySelector('.zindex-active')
+	
 	if (previous && previous2) {
 		previous.classList.remove('round-active')
 		previous2.classList.remove('zindex-active')
+		inputChecked.checked = false
+		
 	}
 	if (e.target.value === '5000') {
 		amount1.parentNode.classList.add('round-active')
 		amount1.parentNode.parentNode.classList.add('zindex-active')
+		inputChecked = true
 	} else if (e.target.value === '2000') {
 		amount2.parentNode.classList.add('round-active')
 		amount2.parentNode.parentNode.classList.add('zindex-active')
+		inputChecked = true
 	} else if (e.target.value === '1000') {
 		amount3.parentNode.classList.add('round-active')
 		amount3.parentNode.parentNode.classList.add('zindex-active')
+		inputChecked = true
 	} else if (e.target.value === '500') {
 		amount4.parentNode.classList.add('round-active')
 		amount4.parentNode.parentNode.classList.add('zindex-active')
+		inputChecked = true
 	} else if (e.target.value === '250') {
 		amount5.parentNode.classList.add('round-active')
 		amount5.parentNode.parentNode.classList.add('zindex-active')
+		inputChecked = true
 	} else if (e.target.value === '100') {
 		amount6.parentNode.classList.add('round-active')
 		amount6.parentNode.parentNode.classList.add('zindex-active')
+		inputChecked = true
 	} else if (e.target.value === '50') {
 		amount7.parentNode.classList.add('round-active')
 		amount7.parentNode.parentNode.classList.add('zindex-active')
+		inputChecked = true
 	} else if (e.target.value === '25') {
 		amount8.parentNode.classList.add('round-active')
 		amount8.parentNode.parentNode.classList.add('zindex-active')
+		inputChecked = true
 	}
 }
 
 inputScale.addEventListener('focusout', changeInputAmount)
-paymentAmountInput.forEach(input => input.addEventListener('click', changeInputAmount))
+// paymentAmountInput.forEach(input => input.addEventListener('click', changeInputAmount))
 
 //highlighting a particular circle after pupulation same amount
 
 function highlightInput(e) {
-	if ((e.value = '5000')) {
-		e.target.id === 'amount-5000'
+	if ((e.target.id === 'amount-5000')) {
+		inputScale.setAttribute('value', '5000')
 	} else if (e.target.id === 'amount-2000') {
 		inputScale.setAttribute('value', '2000')
 	} else if (e.target.id === 'amount-1000') {
@@ -107,3 +119,5 @@ function highlightInput(e) {
 		inputScale.setAttribute('value', '25')
 	}
 }
+
+paymentAmountInput.forEach(input => input.addEventListener('click', highlightInput))
